97704aaeea357155fa5797685c87630b182270b8
lifuhuang97 <54570187+lifuhuang97@users.noreply.github.com>
2020-08-09 18:55:24 +0800

    customizing

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ selectElement('.open').addEventListener('click', () => {
 selectElement('.close').addEventListener('click', () => {
 	selectElement('.nav-list').classList.remove('active');
 });
+
+// Navbar lightbox effect >>> (to be worked on)
+
+// const navbar = document.getElementById('header');
+
+// window.onscroll = function () {
+// 	if (window.pageYOffset < 100) {
+// 		navbar.classList.add('navbar-top');
+// 	} else {
+// 		navbar.classList.remove('navbar-top').add('navbar-not-top');
+// 	}
+// };
